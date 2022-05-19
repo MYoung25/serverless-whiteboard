f850e8ca9432ec51cdd2a6d714d58017f56d74d0
Michael Young <michael25young25@gmail.com>
2022-05-18 20:48:00 -0400

    remove unneeded console.logs

--- src/durableObject.ts
+++ src/durableObject.ts
@@ -27,25 +27,19 @@ export class Counter extends Actor {
         // NOTE: don't actually need this
         this.#wait = state.waitUntil.bind(state);
         this.storage = state.storage
-
-        console.log(Object.keys(this))
     }
 
     async setup(state: Durable.State, bindings: Bindings) {
-        console.log('setup')
         this.count = (await state.storage.get(`count`)) || 0
     }
 
     receive(req: Request): Promise<Response> {
-        console.log('receiver')
-
         return this.API.run(req, {
             waitUntil: this.#wait
         });
     }
 
     onconnect(req: Request, server: WebSocket) {
-        console.log('onconnect')
         this.connections.push({ ws: server })
 
         server.addEventListener(`message`, this.handleMessage)
@@ -56,26 +50,20 @@ export class Counter extends Actor {
     }
 
     router() {
-        console.log('router')
         const API = new Router()
 
         API.add(`GET`, `/`, () => {
-            console.log('GET /')
             return reply(200, this.count)
         })
 
         API.add(`GET`, `/ws`, async (req) => {
-            console.log('/ws')
-            console.log(req)
             const x =  await this.connect(req)
-            console.log(x)
             return x
         })
         return API
     }
 
     broadcast() {
-        console.log('broadcast')
         this.connections.forEach((client) => {
             try {
                 replyWS(client.ws, `count/UPDATED`, this.count)
@@ -84,23 +72,21 @@ export class Counter extends Actor {
     }
 
     handleMessage = async (e) => {
-        console.log('handleMessage', e)
         try {
             const event = JSON.parse(e.data)
-            console.log(event, this.storage)
 
-            // switch (event.type) {
-            //     case 'increment':
-            //         await this.storage.put('count', ++this.count)
-            //         break
+            switch (event.type) {
+                case 'increment':
+                    await this.storage.put('count', ++this.count)
+                    break
 
-            //     case 'decrement':
-            //         await this.storage.put('count', --this.count)
-            //         break
+                case 'decrement':
+                    await this.storage.put('count', --this.count)
+                    break
 
-            //     default:
-            //         break
-            // }
+                default:
+                    break
+            }
 
             this.broadcast()
         } catch (err) {
